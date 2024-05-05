2575ce7ab527e24c0dab7e50e8a8f22829fb2f44
StarX <dmytromaloshtan@gmail.com>
2024-05-05 17:48:34 +0900

    Init

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import './App.css'
+import { Web3WalletProvider } from './common/We3ReactLibs/components/Web3WalletProvider'
+import Layout from './common/layout/Layout'
+import { RefreshContextProvider, OXIProvider } from './contexts'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
+import { OXI } from './pages'
+
+function App() {
+  return (
+    <Web3WalletProvider>
+      <RefreshContextProvider>
+        <BrowserRouter>
+          <OXIProvider>
+            <Layout>
+              <Routes>
+                <Route path="/" element={<OXI />} />
+                {/* <Route path="/home" element={<Home />} /> */}
+                <Route
+                  path="*"
+                  element={<Navigate to="/" replace />}
+                />
+              </Routes>
+            </Layout>
+          </OXIProvider>
+        </BrowserRouter>
+        <ToastContainer />
+      </RefreshContextProvider>
+    </Web3WalletProvider>
+  )
+}
+
+export default App
